a9f355d207390a37c1a269e9cc3fe002cd82636e
Gordian Etim <etimgordian@gmail.com>
2022-10-30 21:14:44 +0100

    final commit

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const app = express();
-const dotenv = require("dotenv");
-dotenv.config()
 
 // This tells the app which port to listen to
-app.listen(process.env.PORT || 5000, () => {
-    console.log(`Server is running on port ${process.env.PORT}`)
+app.listen(5000, () => {
+    console.log(`Server is running on port 5000`)
 })
 //This shows the response that will sent to the user
 app.get("/", (req,res) => {
     res.send('Hello World')
 })
-
